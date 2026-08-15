1dc8c6e0c351d88339eca58cff88485e1652ddd5
Debadrita Mondal <111679707+mondaldebadrita03@users.noreply.github.com>
2026-08-16 01:49:47 +0530

    Create 347_TopKFrequentElements.java

---
+++ 347_TopKFrequentElements.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class Solution {
+    public int[] topKFrequent(int[] nums, int k) {
+        HashMap<Integer, Integer> freq = new HashMap<>();
+
+        for(int num : nums)
+            freq.put(num, freq.getOrDefault(num, 0) + 1);
+
+        List<Integer> elements = new ArrayList<>(freq.keySet());
+
+        elements.sort((a, b) -> freq.get(b) - freq.get(a));
+
+        int[] ans = new int[k];
+
+        for(int i = 0; i < k; i++)
+            ans[i] = elements.get(i);
+
+        return ans;
+    }
+}
