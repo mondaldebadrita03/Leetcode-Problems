d170a2d3cef26898239f72423e94f73921ed127e
Debadrita Mondal <111679707+mondaldebadrita03@users.noreply.github.com>
2026-08-27 18:32:29 +0530

    Create 2704_ToBeOrNotToBe.js

---
+++ 2704_ToBeOrNotToBe.js
@@ -0,0 +1,16 @@
+var expect=function(val){
+    return {
+        toBe:function(value){
+            if(val===value){
+                return true;
+            }
+            throw new Error("Not Equal");
+        },
+        notToBe:function(value){
+            if(val!==value){
+                return true;
+            }
+            throw new Error("Equal");
+        }
+    }
+ }
